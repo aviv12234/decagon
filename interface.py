23b9bf58306972c4c9dfc31af55d781b46d38a7a
aviv12234 <58222380+aviv12234@users.noreply.github.com>
2022-05-08 14:46:10 +0300

    added csv file writing

--- interface.py
+++ interface.py
@@ -5,24 +5,26 @@ from PIL import ImageTk
 from tkinter import *
 from PIL import Image
 import validators
+import csv
+
 
 
 window = tk.Tk()
 
+data_file = 'Url_Data.csv'
 
 
+gif_file="images/background.gif"
 
-file="images/background.gif"
+info = Image.open(gif_file)
 
-info = Image.open(file)
-
-
+header = ['Url', 'Sql_injection', 'Xss']
 
 
 frames = info.n_frames  # gives total number of frames that gif contains
 
 # creating list of PhotoImage objects for each frames
-im = [tk.PhotoImage(file=file,format=f"gif -index {i}") for i in range(frames)]
+im = [tk.PhotoImage(file=gif_file,format=f"gif -index {i}") for i in range(frames)]
 
 count = 0
 def animation(count):
@@ -40,11 +42,30 @@ gif_label.pack()
 
 animation(count)
 
+def has_header(filename):
+    with open(filename) as f:
+        
+        txt_file = f.read().split(',')
+
+
+        for tab in range(len(header)):
+            if txt_file[tab].strip() != header[tab]:
+                return False
+    return True
+        
+
+        
+
+
+
+
 
 def printInput():
     inp = inputtxt.get(1.0, "end-1c")
 
     valid=validators.url(inp)
+
+
 
     if valid:
         xss_weakness = xss_detector.scan_xss(inp)
@@ -63,11 +84,31 @@ def printInput():
         
 
 
+
     else:
         lbl.config(fg = "yellow")
         lbl.config(text = "not a valid target")
 
 
+    
+    data_add = [inp, xss_detector.scan_xss(inp), sql_injection_detector.scan_sql_injection(inp)]
+    header = ['Url', 'Sql_injection', 'Xss']
+
+    data = open(data_file, 'a', newline="")
+
+
+    if not has_header(data_file):
+        for header in header:
+            data.write(str(header)+', ')
+
+
+
+
+    data.write('\n')
+    for variable in data_add:
+        data.write(str(variable)+', ')
+
+    data.close()
 
 
 
@@ -90,7 +131,7 @@ printButton = tk.Button(window,
                         command = printInput)
 printButton.pack()
 
-lbl = tk.Label(window, text = "", bg = "black", fg = "red")
+lbl = tk.Label(window,  text = "", bg = "black", fg = "red")
 lbl.pack()
 
 
